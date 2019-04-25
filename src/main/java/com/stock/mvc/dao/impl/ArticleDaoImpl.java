8cf7cd58da6b1196b01afe332f6d8daec39c1925
Makadji <Makadji@Makadji-PC>
2019-04-25 16:00:06 +0100

    chargement des services commit

--- src/main/java/com/stock/mvc/dao/impl/ArticleDaoImpl.java
+++ src/main/java/com/stock/mvc/dao/impl/ArticleDaoImpl.java
@@ -3,6 +3,7 @@ package com.stock.mvc.dao.impl;
 import com.stock.mvc.bean.Article;
 import com.stock.mvc.dao.ArticleDao;
 
-public class ArticleDaoImpl extends GenericDaoImpl<Article> implements ArticleDao{
+public class ArticleDaoImpl extends GenericDaoImpl<Article> implements ArticleDao {
 
 }
+
