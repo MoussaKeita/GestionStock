ec7b5ae96065b0ca663490cee66f3af4888060a5
Keita Moussa <moussakeitamoussakeita@gmail.com>
2019-04-28 17:41:26 +0100

    liste des clients avec l'icone des actions ajouter, supprimer,modifier, importer extporter

---
+++ src/main/java/com/stock/mvc/controllers/ClientController.java
@@ -0,0 +1,29 @@
+package com.stock.mvc.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stock.mvc.bean.Client;
+import com.stock.mvc.service.ClientService;
+
+@Controller
+@RequestMapping(value="/client")
+public class ClientController {
+	@Autowired
+	private ClientService clientService;
+	
+	@RequestMapping(value="/")
+	public String client(Model model) {
+		List<Client> clients =clientService.selectAll();	
+		if(clients==null) {
+			clients =new ArrayList<Client>();
+		}
+		model.addAttribute("clients",clients);
+		return "client/client";
+	}
+}
